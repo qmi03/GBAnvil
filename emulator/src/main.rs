90c652e7258dc908942b8bdd112261806cc81532
Qmi <minh.pham1126@gmail.com>
2024-05-26 00:22:58 +0700

    feat: init rust project

---
+++ emulator/src/main.rs
@@ -0,0 +1,14 @@
+#[derive(Debug)]
+struct Registers {
+    a: u8;
+    b: u8;
+    c: u8;
+    d: u8;
+    e: u8;
+    f: u8;
+    h: u8;
+    l: u8;
+}
+fn main() {
+    println!("Hello, world!");
+}
